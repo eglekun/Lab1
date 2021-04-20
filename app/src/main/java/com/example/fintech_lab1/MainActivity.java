a397d489b371dc7c3a7fb906310ed740f5efe8e8
Egle <kundrotaite.egle@gmail.com>
2021-04-20 23:26:03 +0300

    Added button for changing text color

--- app/src/main/java/com/example/fintech_lab1/MainActivity.java
+++ app/src/main/java/com/example/fintech_lab1/MainActivity.java
@@ -2,6 +2,7 @@ package com.example.fintech_lab1;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.graphics.Color;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.TextView;
@@ -20,5 +21,11 @@ public class MainActivity extends AppCompatActivity {
 
     public void OnBtnClick(View view) {
         this.tvMain.setText(R.string.changed_label_caption);
+    }
+
+    public void OnBtnClickChangeColor(View view) {
+        this.tvMain.setTextColor(Color.RED);
+        this.tvMain.setBackgroundColor(Color.GRAY);
+        this.tvMain.setText(R.string.changed_color_label_caption);
     }
 }
